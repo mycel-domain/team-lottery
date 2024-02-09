82050032b3954216ee4f11f393931f762d6a8a47
shuhei tanaka <67859510+tnkshuuhei@users.noreply.github.com>
2024-02-09 15:49:04 +0900

    constructor

--- src/Vault.sol
+++ src/Vault.sol
@@ -10,8 +10,32 @@ import {SafeCast} from "openzeppelin-contracts/contracts/utils/math/SafeCast.sol
 import {Math} from "openzeppelin-contracts/contracts/utils/math/Math.sol";
 import {Ownable} from "owner-manager-contracts/Ownable.sol";
 
-contract Vault {
+import {PrizePool} from "./PrizePool.sol";
+
+contract Vault is ERC20, ERC20Permit, Ownable {
+    /// @notice Address of the ERC4626 vault generating yield.
+    IERC4626 private immutable _yieldVault;
+
+    /// @notice Address of the PrizePool that computes prizes.
+    PrizePool private immutable _prizePool;
+
+    /// @notice Address of the underlying asset used by the Vault.
+    IERC20 private immutable _asset;
+
     using Math for uint256;
     using SafeCast for uint256;
     using SafeERC20 for IERC20;
+
+    constructor(
+        IERC20 asset_,
+        string memory name_,
+        string memory symbol_,
+        IERC4626 yieldVault_,
+        PrizePool prizePool_,
+        address owner_
+    ) ERC20(name_, symbol_) ERC20Permit(name_) Ownable(owner_) {
+        _asset = asset_;
+        _yieldVault = yieldVault_;
+        _prizePool = prizePool_;
+    }
 }
